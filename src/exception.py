4f6f2b658b16f02fd5350ff4c15580321a309801
Muneeb Abro <muneebabr000@gmail.com>
2025-07-06 16:24:50 +0500

    project structure

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys 
-import logging
+from logger import *
+import logger
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -22,3 +23,10 @@ class CustomException(Exception):
         return CustomException.__name__.str()
     
 
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.error(e)
+        raise CustomException(e, sys)
+        
--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 
+
 LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.log"
 logs_dir = os.path.join(os.getcwd(), "logs")
 os.makedirs(logs_dir, exist_ok=True)
@@ -11,7 +12,7 @@ LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)  # ✅ Just dir + filename
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format='%(asctime)s - %(levelname)s - %(message)s',
-    level=logging.INFO,
+    level=logging.DEBUG,
     filemode='w'
 )
 
